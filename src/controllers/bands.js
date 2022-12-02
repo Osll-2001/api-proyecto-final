434d7cf39155066d01e3d735fae081e0aeba1793
octavio <octaviosuriellopezlopez@gmail.com>
2022-12-01 20:53:00 -0600

    Implementación: delete Bands

--- src/controllers/bands.js
+++ src/controllers/bands.js
@@ -29,6 +29,15 @@ const BandsController = {
     } catch (err) {
       res.status(500).json({ message: err.message })
     }
+  },
+  delete: async (req, res) => {
+    const { id } = req.params
+    try {
+      await BandsService.delete(id)
+      res.status(200).json({ message: 'Banda Eliminada' })
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
   }
 }
 
--- src/routes/bands.js
+++ src/routes/bands.js
@@ -5,5 +5,6 @@ const BandsController = require('../controllers/bands')
 BandsRouter.get('/', BandsController.get)
 BandsRouter.get('/:id', BandsController.getById)
 BandsRouter.post('/', BandsController.create)
+BandsRouter.delete('/:id', BandsController.delete)
 
 module.exports = BandsRouter
